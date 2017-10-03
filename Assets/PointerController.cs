32111e293e852848064d7489f46f2ab3e43bfd60
LeeviPelkonen <Leevi.pelkonen@metropolia.fi>
2017-10-03 13:30:16 +0300

    Revert "animation update"
    This reverts commit 8dbb519668ec9e1a9ea9258c1e75fbb11d731d85.

---
+++ Assets/PointerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private bool pressed;
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		pressed = false;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		//Debug.Log ("Pressed a button");
+		//Debug.Log (eventData);
+		pressed = true;
+	}
+
+	public bool GetPressed(){return pressed;}
+}
