6b53f579e849869bb02c4555a40defb9dcb6d6e9
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2017-10-09 16:16:39 +0300

    audio update + folders
    audio update
    + sorting files to different folders

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class GameItem: MonoBehaviour
+{
+	private string itemName;
+	private string imageName;
+	public GameItem (string itemName,string imageName)
+	{
+		this.itemName = itemName;
+		this.imageName = imageName;
+	}
+
+	public string GetName(){
+		return this.itemName;
+	}
+}
+
+
---
+++ Assets/Scripts/PickableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{	
+	void OnTriggerEnter(Collider col)
+	{
+		Player.inventory.Add (new GameItem (this.name, this.name));
+		GameObject.Find (this.name).SetActive (false);
+		if (this.name.Equals ("Map")) {
+			MainController.MapButtonVisibility ();
+		}
+	}
+}
