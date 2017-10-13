60918d76ca372a37ae64cba359c059f882b91f89
Joonas-Aaltonen <32293674+Joonas-Aaltonen@users.noreply.github.com>
2017-10-13 11:28:59 +0300

    Updated Cutscene
    - Added comments

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -5,14 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Cutscene : MonoBehaviour
 {
-	public Texture[] frames;
-	public float framesPerSecond;
+	public Texture[] frames;	//Simple array of textures
+	public float secondsPerFrame;	//How many seconds reserved for each frame
 
 	public int currentScene;
 
 	private int frame = 0;
 	private float nextFrame = 0f;
-	void Start () {
+	void Start ()//Setting everything to 0, just in case.
+	{
 		Screen.orientation = ScreenOrientation.Landscape;
 		frame = 0;
 		nextFrame = 0f;
@@ -20,18 +21,19 @@ public class Cutscene : MonoBehaviour
 	}
 	void OnGUI()
 	{		
-		
-		if (this.frame < this.frames.Length) 
+		//Checks whether the current frame is the last frame or not
+		if (this.frame < this.frames.Length)
 		{
 			if ( Time.timeSinceLevelLoad >= nextFrame)
 			{
 				this.frame++;
 				nextFrame += framesPerSecond;
 			}
+			//Draws the textures in the array to the canvas
 			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.frames [this.frame - 1]);
 		}
 		else 
-		{	
+		{	//If current scene is not the ending
 			if (currentScene < 3) 
 			{
 				SceneManager.LoadScene (currentScene + 1);
